4d3a2455aba48bbd20bb4397b61fa1e078df6fea
AymanHassan <ayman.hassan.temraz@gmail.com>
2026-06-09 11:21:20 +0300

    AddGlobalErrorHandlerMiddleware

---
+++ EmergencySystem.API/Middlewares/GlobalErrorHandlerMiddelware.cs
@@ -0,0 +1,23 @@
+﻿namespace EmergencySystem.API.Middlewares;
+
+public class GlobalErrorHandlerMiddelware 
+{
+    private readonly RequestDelegate _next;
+
+    public GlobalErrorHandlerMiddelware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+             await _next.Invoke(context);
+        }
+        catch (Exception ex)
+        {
+            throw;
+        }
+    }
+}
--- EmergencySystem.API/Program.cs
+++ EmergencySystem.API/Program.cs
@@ -25,6 +25,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseMiddleware<GlobalErrorHandlerMiddelware>();
+
 app.UseMiddleware<TransactionMiddleware>();
 
 app.UseHttpsRedirection();
